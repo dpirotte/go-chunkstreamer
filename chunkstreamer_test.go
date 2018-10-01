729c00841581c8a171e5a487d6a575477810ab11
Dave Pirotte <dpirotte@gmail.com>
2018-10-01 11:56:04 -0500

    Add some benchmarks comparing pulling small strings from a chunkstreamer to "just" serializing those strings as a large JSON array

--- chunkstreamer_test.go
+++ chunkstreamer_test.go
@@ -3,6 +3,7 @@ package chunkstreamer_test
 import (
 	"bytes"
 	"crypto/rand"
+	"encoding/json"
 	"io"
 	"testing"
 
@@ -76,3 +77,67 @@ func benchmarkWrite(size int, b *testing.B) {
 
 func BenchmarkWrite1(b *testing.B)  { benchmarkWrite(1, b) }
 func BenchmarkWrite10(b *testing.B) { benchmarkWrite(10, b) }
+
+func benchmarkReadWriteBatch(size int, b *testing.B) {
+	var (
+		msgs [][]byte
+		msg  = []byte("This is a relatively short string message.")
+	)
+
+	for i := 0; i < size; i++ {
+		msgs = append(msgs, msg)
+	}
+
+	for i := 0; i < b.N; i++ {
+		var buf bytes.Buffer
+		w := chunkstreamer.NewWriter(&buf)
+
+		for _, s := range msgs {
+			w.Write([]byte(s))
+		}
+
+		r := chunkstreamer.NewReader(&buf)
+		for {
+			_, err := r.ReadChunk()
+			if err == io.EOF {
+				break
+			}
+		}
+	}
+}
+
+func BenchmarkReadWriteBatch10(b *testing.B)      { benchmarkReadWriteBatch(10, b) }
+func BenchmarkReadWriteBatch1000(b *testing.B)    { benchmarkReadWriteBatch(1000, b) }
+func BenchmarkReadWriteBatch100000(b *testing.B)  { benchmarkReadWriteBatch(100000, b) }
+func BenchmarkReadWriteBatch1000000(b *testing.B) { benchmarkReadWriteBatch(1000000, b) }
+
+func benchmarkReadWriteBatchJSON(size int, b *testing.B) {
+	var (
+		msgs [][]byte
+		msg  = []byte("This is a relatively short string message.")
+	)
+
+	for i := 0; i < size; i++ {
+		msgs = append(msgs, msg)
+	}
+
+	for i := 0; i < b.N; i++ {
+		j, err := json.Marshal(msgs)
+		if err != nil {
+			panic(err)
+		}
+
+		var buf bytes.Buffer
+		buf.Write(j)
+
+		var parsedMsgs [][]byte
+		err = json.Unmarshal(buf.Bytes(), &parsedMsgs)
+
+		for _, _ = range parsedMsgs {
+		}
+	}
+}
+func BenchmarkReadWriteBatchJSON10(b *testing.B)      { benchmarkReadWriteBatchJSON(10, b) }
+func BenchmarkReadWriteBatchJSON1000(b *testing.B)    { benchmarkReadWriteBatchJSON(1000, b) }
+func BenchmarkReadWriteBatchJSON100000(b *testing.B)  { benchmarkReadWriteBatchJSON(100000, b) }
+func BenchmarkReadWriteBatchJSON1000000(b *testing.B) { benchmarkReadWriteBatchJSON(1000000, b) }
